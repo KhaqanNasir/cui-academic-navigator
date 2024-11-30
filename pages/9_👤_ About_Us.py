9f4e03cfd745928935df45c3d80bb3cfcbcbace4
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2024-11-30 22:43:54 +0500

    Update 9_👤_ About_Us.py

--- pages/9_👤_ About_Us.py
+++ pages/9_👤_ About_Us.py
@@ -42,14 +42,14 @@ def main():
             "registration_no": "FA22-BCS-038",
             "github": "#",
             "linkedin": "#",
-            "image": os.path.join(os.path.dirname(__file__), 'assets', 'member2.jpg')  # Path to local image
+            "image": os.path.join(os.path.dirname(__file__), 'assets', 'Profile_Pic_Updat.jpg')  # Path to local image
         },
         {
             "name": "🖥️ Ahsan Abdullah",
             "registration_no": "FA22-BCS-015",
             "github": "#",
             "linkedin": "#",
-            "image": os.path.join(os.path.dirname(__file__), 'assets', 'member3.jpg')  # Path to local image
+            "image": os.path.join(os.path.dirname(__file__), 'assets', 'Profile_Pic_Updat.jpg')  # Path to local image
         }
     ]
     
@@ -67,12 +67,10 @@ def main():
                     <div style='font-size: 22px; font-weight: 600; color: #5F6366;'>{member['name']}</div>
                     <div style='font-size: 18px; color: #5F6366;'>{member['registration_no']}</div>
                     <div style='margin-top: 10px;'>
-                        <a href='https://github.com/{member["github"]}' target='_blank' style='text-decoration: none; font-size: 14px;'>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" style="fill:#000000;">
-                                <path d="M12 2C6.48 2 2 6.48 2 12c0 4.99 3.67 9.17 8.5 10.64v-7.55h-2.56v-2.62h2.56v-2c0-2.53 1.5-3.93 3.84-3.93 1.11 0 2.28.08 2.58.12v2.85h-1.73c-1.35 0-1.79.84-1.79 1.69v2.16h3.04l-.49 2.62h-2.55v7.58c4.83-.83 8.5-4.9 8.5-9.64 0-5.52-4.48-10-10-10z"/>
-                            </svg>
-                            GitHub
+                        <a href='https://github.com/{member["github"]}' target='_blank' class='social-button'>
+                           <i class="fab fa-github" style="font-size: 20px; color: #000000;"></i> GitHub
                         </a>
+
                         <a href='https://linkedin.com/in/{member["linkedin"]}' target='_blank' style='text-decoration: none; font-size: 14px; color: #007bff; margin-left: 10px;'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" style="fill:#007bff;">
                                 <path d="M19 0H5C3.9 0 3 .9 3 2v20c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM8.5 17h-2v-7h2v7zM7.5 9.75c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM18 17h-2v-3.5c0-1.12-.91-2-2-2s-2 .88-2 2V17h-2v-7h2v1.09c.34-.62 1.04-1.09 1.89-1.09 1.48 0 2.61 1.25 2.61 2.61V17z"/>
