c24e8f3d2eed4933a4ac5a5f0f52eeb45c10466d
Muhammad Khaqan Nasir <khaqannasir01@gmail.com>
2025-01-15 00:28:14 +0500

    Update 4_📝_Study_Notes_Maker.py

--- pages/4_📝_Study_Notes_Maker.py
+++ pages/4_📝_Study_Notes_Maker.py
@@ -141,7 +141,7 @@ def main():
     # Footer
     st.markdown("---")
     st.markdown(
-      '<p style="text-align: center; font-weight: 600; font-size: 16px;">💻 Developed with ❤️ using Streamlit | © 2024</p>',
+      '<p style="text-align: center; font-weight: 600; font-size: 16px;">💻 Developed with ❤️ using Streamlit | © 2025</p>',
        unsafe_allow_html=True)
 
 if __name__ == "__main__":
